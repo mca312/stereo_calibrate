be2077b7f9537faf078c41552db348ac14ff53da
angellotti <mangellotti@wyzant.com>
2020-10-19 21:31:20 -0500

    clean up

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import cv2
 from camera import Cameras
 import matplotlib.pyplot as plt
 
+DIR_NAME = 'chessboard03'
+CHESSBOARD_SIZE = (9, 6)
+
 def main():
   """ entry point  """
   test_calibrate() # uses photos
 
 def test_calibrate():
   criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-  chessboard_size = (8, 6)
   obj_points = []
   img_points_r = []
   img_points_l = []
@@ -23,8 +25,8 @@ def test_calibrate():
   corners_l = None
   corners_r = None
 
-  objp = np.zeros((np.prod(chessboard_size), 3), dtype=np.float32)
-  objp[:, :2] = np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1, 2)
+  objp = np.zeros((np.prod(CHESSBOARD_SIZE), 3), dtype=np.float32)
+  objp[:, :2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1, 2)
 
   for i in range(1, 113):
     t = str(i)
@@ -32,12 +34,12 @@ def test_calibrate():
     chessboard_r_found = False
     corners_l = None
     corners_r = None
-    chess_img_l = cv2.imread('chessboard02/chessboard-L'+t+'.png', 0)
-    chess_img_r = cv2.imread('chessboard02/chessboard-R'+t+'.png', 0)
+    chess_img_l = cv2.imread('{0}/chessboard-L{1}.png'.format(DIR_NAME, t), 0)
+    chess_img_r = cv2.imread('{0}/chessboard-R{1}.png'.format(DIR_NAME, t), 0)
     img_shape = chess_img_r.shape[:2]
     try:
-      chessboard_l_found, corners_l = cv2.findChessboardCorners(chess_img_l, chessboard_size, None)
-      chessboard_r_found, corners_r = cv2.findChessboardCorners(chess_img_r, chessboard_size, None)
+      chessboard_l_found, corners_l = cv2.findChessboardCorners(chess_img_l, CHESSBOARD_SIZE, None)
+      chessboard_r_found, corners_r = cv2.findChessboardCorners(chess_img_r, CHESSBOARD_SIZE, None)
     except Exception as ex:
       print('error finding chessboard {0}'.format(t))
 
@@ -72,6 +74,8 @@ def test_calibrate():
   rot_l, rot_r, proj_l, proj_r, Q, roi_l, roi_r = cv2.stereoRectify(matrix_l, dist_coef_l, matrix_r, dist_coef_r, img_shape, R, T)
 
   print('retS.stereoCalibrate - ', retS, ' retL - ', ret_l, ' retR - ', ret_r)
+  print('R: ', R)
+  print('T:', T)
 
   left_map = cv2.initUndistortRectifyMap(matrix_l, dist_coef_l, rot_l, new_camera_matrix_l, img_shape, cv2.CV_16SC2)
   right_map = cv2.initUndistortRectifyMap(matrix_r, dist_coef_r, rot_r, new_camera_matrix_r, img_shape, cv2.CV_16SC2)
@@ -79,26 +83,6 @@ def test_calibrate():
   stereo_map = (left_map, right_map)
   with open('stereo_map.pickle', 'wb') as handle:
     pickle.dump(stereo_map, handle, protocol=HIGHEST_PROTOCOL)
-
-  window_size = 5
-  min_disp = 15
-  num_disp = 191-min_disp
-  blockSize = window_size
-  uniquenessRatio = 1
-  speckleRange = 3
-  speckleWindowSize = 3
-  disp12MaxDiff = 200
-  stereo = cv2.StereoSGBM_create(
-    minDisparity = min_disp,
-    numDisparities = num_disp,
-    blockSize = window_size,
-    uniquenessRatio = uniquenessRatio,
-    speckleRange = speckleRange,
-    speckleWindowSize = speckleWindowSize,
-    disp12MaxDiff = disp12MaxDiff,
-    P1=8*3*window_size**2,#8*3*win_size**2,
-    P2=32*3*window_size**2
-  )
 
   cameras = Cameras()
   while True:
@@ -111,16 +95,7 @@ def test_calibrate():
     ret = cv2.getValidDisparityROI(roi_l, roi_r, 1, 10, 5)
     print(ret)
     undistort_l = cv2.undistort(frame_l, K_l, dist_l, None, new_camera_matrix_l)
-    # x,y,w,h = ret
-    # undistort_l = undistort_l[y:y+h, x:x+w]
     undistort_r = cv2.undistort(frame_r, K_r, dist_r, None, new_camera_matrix_r)
-    # x,y,w,h = ret
-    # undistort_r = undistort_r[y:y+h, x:x+w]
-
-    # disp = stereo.compute(frame_l, frame_r).astype(np.float32) / 16.0
-    # cv2.imshow('disparity', (disp-min_disp)/num_disp)
-
-    # res = np.hstack((undistort_l, undistort_r))
     cv2.imshow('undistort_l', undistort_l)
     cv2.imshow('undistort_r', undistort_r)
 
@@ -129,21 +104,25 @@ def test_calibrate():
     cv2.imshow('left_rectified', left_rectified)
     cv2.imshow('right_rectified', right_rectified)
 
-    #Generate  point cloud. 
-    # h, w = undistort_r.shape[:2]
-    # focal_length = 6
-    # Q2 = np.float32([[1, 0, 0, 0],
-    #                  [0, -1, 0, 0],
-    #                  [0, 0, focal_length*0.05, 0], #Focal length multiplication obtained experimentally.
-    #                  [0, 0, 0, 1]])
+    img_to_use_l = left_rectified
+    img_to_use_r = right_rectified
 
-    # points_3D = cv2.reprojectImageTo3D(disparity_map, Q2)
-    # mask_map = disparity_map > disparity_map.min()
+    chessboard_l_found, corners_l = cv2.findChessboardCorners(img_to_use_l, CHESSBOARD_SIZE, None)
+    chessboard_r_found, corners_r = cv2.findChessboardCorners(img_to_use_r, CHESSBOARD_SIZE, None)
+    print(chessboard_l_found, chessboard_r_found)
+    if chessboard_l_found == True & chessboard_r_found == True:
+      print('point_l: ', corners_l[0][0][0])
+      print('point_r: ', corners_r[0][0][0])
+      disparity_maybe = corners_l[0][0][0] - corners_r[0][0][0]
+      disp_may = (corners_l[0][0][0], corners_l[0][0][1], disparity_maybe)
+      np_disp = np.array([[[corners_l[0][0][0], corners_l[0][0][1], disparity_maybe]]], dtype=np.float32)
+      new_coord = cv2.perspectiveTransform(np_disp, Q)
+      print('new_coord: ', new_coord)
 
     k = cv2.waitKey(0) & 0xFF
     if k == 115:
-      cv2.imwrite('chessboard02/img-L.png', frame_l)
-      cv2.imwrite('chessboard02/img-R.png', frame_r) # Save the image in the file where this Programm is located
+      cv2.imwrite('{0}/img-L.png'.format(DIR_NAME), frame_l)
+      cv2.imwrite('{0}/img-R.png'.format(DIR_NAME), frame_r)
     if k == 27:
       break
 
